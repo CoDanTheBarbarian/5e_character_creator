72f205ec11c30885dba134dc978e283e7967219e
CoDanTheBarbarian <plafcandaniel@gmail.com>
2024-10-25 14:53:19 -0700

    2: Random core stat roller implemented in the CLI.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from character.character_objects import *
+from random_stats import *
 def parse_name(name):
     response = input("Character name: " + name + "\nConfirm? (y/n) ")
     if response == "n":
@@ -9,16 +10,30 @@ def parse_name(name):
     else:
         print("Please enter 'y' or 'n'")
         parse_name(name)
+
+def choose_stats(stats):
+    print(f"You rolled: {stats}")
+    response = input("Would you like to roll again? (y/n) ")
+    if response == "y":
+        stats = get_random_stats()
+        choose_stats(stats)
+    elif response == "n":
+        print("With stats out of the way, let's choose a race and class!")
+        print("We'll assign stats later.")
+    else:
+        print("Please enter 'y' or 'n'")
+        choose_stats(stats)
+
+
 def main():
     print("Welcome to my character creator. Let's make a new character!")
     name = input("First things first, what is your new character's name? ")
     parse_name(name)
     character = Character(name)
-
-
-
-
-
+    print("Before anything else, let's roll some random stats!")
+    stats = get_random_stats()
+    choose_stats(stats)
+    print("Choose your race:")
     
 if __name__ == "__main__":
     main()
